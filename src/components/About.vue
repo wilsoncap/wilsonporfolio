300715251252106f411c83bdd05783548a0cda53
wilson capacho <wiston.98@htomail>
2025-08-06 14:08:47 -0500

    ajuste en aboutme

--- src/components/About.vue
+++ src/components/About.vue
@@ -9,7 +9,7 @@
             ¡Hola! Soy Wilson Capacho, desarrollador Full Stack proactivo, autodidacta y apasionado por el desarrollo web. Tengo más de 2 años de experiencia creando soluciones eficientes y escalables con tecnologías como Laravel, JavaScript, Vue.js y PostgreSQL.
           </p>
           <p class="text-lg md:text-xl text-left max-w-2xl">
-            He desarrollado módulos para ERPS, trabajado en integraciones con APIs REST y SOAP, y contribuido a mejorar la calidad del código en equipos de trabajo. Me destaco por mi enfoque en buenas prácticas, aprendizaje continuo y compromiso con la calidad del software.
+            He desarrollado módulos para ERPs, trabajado en integraciones con APIs REST y SOAP, y contribuido a mejorar la calidad del código en equipos de trabajo. Me adapto rápidamente a nuevos entornos, soy abierto al cambio y al aprendizaje continuo. Me destaco por mi enfoque en buenas prácticas, mejora constante y compromiso con la calidad del software.
           </p>
           <p class="text-lg md:text-xl text-left max-w-2xl">
             Actualmente estoy por finalizar mis estudios en Ingeniería de Sistemas. Me entusiasma seguir creciendo profesionalmente y participar en proyectos donde pueda generar impacto real.
