60ce04958ec6b38a0de82523c1878ff8dd0eb137
wilson capacho <wiston.98@htomail>
2025-08-05 23:59:52 -0500

    ajustes en about, nabvar y conctacto

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,10 +1,10 @@
 <template>
   <nav class="flex items-center justify-between py-4 px-8 bg-card shadow-md">
-    <h1 class="text-2xl font-bold text-primary">Mi Portafolio</h1>
+    <h1 class="text-sm md:text-base lg:text-2xl font-bold text-primary">Mi Portafolio</h1>
     <div class="flex items-center text-white">
-      <a href="#about" class="ml-6 text-lg hover:text-primary transition-colors">Sobre mí</a>
-      <a href="#projects" class="ml-6 text-lg hover:text-primary transition-colors">Proyectos</a>
-      <a href="#contact" class="ml-6 text-lg hover:text-primary transition-colors">Contacto</a>
+      <a href="#about" class="ml-6 text-sm md:text-base lg:text-xl hover:text-primary transition-colors">Sobre mí</a>
+      <a href="#projects" class="ml-6 text-sm md:text-base lg:text-xl text-lg hover:text-primary transition-colors">Proyectos</a>
+      <a href="#contact" class="ml-6 text-sm md:text-base lg:text-xl text-lg hover:text-primary transition-colors">Contacto</a>
     </div>
   </nav>
 </template>
